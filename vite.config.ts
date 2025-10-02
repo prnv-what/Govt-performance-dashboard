b4de41975ec6d60fe2b6bd37f1372428a0f57035
pranavlatka2@gmmail.com <pranav>
2025-10-02 19:40:35 +0530

    made some changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
-  export default defineConfig({
-    plugins: [react()],
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? "/govt-performance-dashboard/" : "/",
+  plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
@@ -49,10 +50,10 @@
         '@': path.resolve(__dirname, './src'),
       },
     },
-    build: {
-      target: 'esnext',
-      outDir: 'build',
-    },
+  build: {
+    target: 'esnext',
+    outDir: 'dist',
+  },
     server: {
       port: 3000,
       open: true,
